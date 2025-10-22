59e456c8ecd6c29f841ed418e1ac8caa4a946454
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-10-22 11:03:52 +0500

    more translates

--- src/components/GamesUidsModal.vue
+++ src/components/GamesUidsModal.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { i18n } from "@/i18n";
 import { useToast } from "vue-toastification"
 
 const props = defineProps<{
@@ -10,7 +11,7 @@ const toast = useToast()
 
 function copy(uid: string) {
   navigator.clipboard.writeText(uid)
-  toast.info("Скопировано "+uid)
+  toast.info(i18n.get("copied", uid))
 }
 
 </script>
@@ -26,8 +27,8 @@ function copy(uid: string) {
     <table class="w-full table-fixed border-collapse text-left">
       <thead class="border-b border-gray-700 text-gray-400">
         <tr>
-          <th class="py-2 px-1">Игра</th>
-          <th class="py-2 px-1">UID</th>
+          <th class="py-2 px-1">{{ i18n.use("games.game") }}</th>
+          <th class="py-2 px-1">{{ i18n.use("games.uid") }}</th>
           <th class="py-2 px-1 w-8"></th>
         </tr>
       </thead>
@@ -36,7 +37,7 @@ function copy(uid: string) {
           <td class="py-2 px-1">{{ game }}</td>
           <td class="py-2 px-1 font-mono">{{ uid }}</td>
           <td class="py-2 px-1 text-center">
-            <button @click="copy(uid)" class="hover:opacity-80 cursor-pointer flex" title="Скопировать UID">
+            <button @click="copy(uid)" class="hover:opacity-80 cursor-pointer flex" :title="i18n.use('games.copy').value">
               <span class="material-icons text-gray-300">content_copy</span>
             </button>
           </td>
