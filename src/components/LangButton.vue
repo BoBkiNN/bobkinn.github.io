e138f1dcfca55c4f96913740775048166ddb20db
BoBkiNN <78136575+BoBkiNN@users.noreply.github.com>
2025-10-25 18:00:07 +0500

    update lang button

--- src/components/LangButton.vue
+++ src/components/LangButton.vue
@@ -4,15 +4,26 @@ import 'floating-vue/dist/style.css'
 
 import { i18n } from '@/i18n'
 import { useLangStore } from '@/stores/langStore'
+import { ref } from 'vue'
 
 const store = useLangStore()
 
 // Extract available languages dynamically from i18n data
 const availableLangs = i18n.languages
+
+const dropdownRef = ref<InstanceType<typeof Dropdown>>()
+
+function setLang(lang: string) {
+  store.lang = lang
+  dropdownRef.value?.hide()
+}
 </script>
 
 <template>
-  <Dropdown class="size-min" theme="lang-dropdown" placement="bottom" :triggers="['hover']" :distance="6" :popperTriggers="['hover']">
+  <Dropdown ref="dropdownRef" class="size-min" theme="lang-dropdown"
+  placement="bottom" :triggers="['hover']"
+  :distance="6" :popperTriggers="['hover']"
+  :autoHide="false">
     <!-- Button -->
     <span class="material-icons text-white p-1 rounded-md bg-[#373737]
     cursor-pointer shadow-2xl hover:bg-(--color-accent) transition">
@@ -24,7 +35,9 @@ const availableLangs = i18n.languages
     <template #popper>
       <div class="bg-[#1e1e1e] text-white rounded-md overflow-hidden min-w-[140px]">
         <div v-for="lang in availableLangs" :key="lang"
-          class="px-3 py-2 hover:bg-[#373737] cursor-pointer flex justify-between gap-1" @click="store.lang = lang">
+          class="px-3 py-2 hover:bg-[#373737] cursor-pointer flex justify-between gap-1"
+          @click="setLang(lang)"
+          :class="{'bg-[#313131]': store.lang == lang}">
           <span>{{ lang }}</span>
           <span class="opacity-80">{{ i18n.getExact('lang.original_name', lang) }}</span>
         </div>
